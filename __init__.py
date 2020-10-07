46415e58dbc3e40295a32b3be9c913678719dd69
lishaogang <17631288710@163.com>
2020-10-07 13:38:04 +0800

    update
    update

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @author: lishaogang
+# @file: __init__.py.py
+# @time: 2020/9/22 0022 17:43
+# @desc:
+
+
+
+if __name__ == '__main__':
+    pass
---
+++ utils/pfget.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @author: lishaogang
+# @file: pfget.py
+# @time: 2020/10/5 0005 18:46
+# @desc:
+
+def get_pflist(file_name):
+    '''
+    从文件中读取帕累托前沿数据
+    :param file_name:
+    :return:
+    '''
+    pf_l = []
+    with open(file_name, 'r', encoding='utf-8') as f:
+        content = f.readlines()
+        for line in content:
+            pf_l.append(list(map(float, line.strip().split('\t'))))
+    return pf_l
+
+if __name__ == '__main__':
+    pass
